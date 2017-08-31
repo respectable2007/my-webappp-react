c9cf0631a429c709aafedccb245acd3e042f1daa
System Administrator <root@zhanghuideMacBook-Air.local>
2017-08-31 17:13:34 +0800

    JSX

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,25 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  // ReactDOM.render(<App />, div);
 });
+
+function formatName(user) {
+	return user.firstName + '•' + user.lastName;
+}
+
+const user = {
+	firstName: 'Harper',
+	lastName: 'Perez'
+}
+
+const element = {
+	<h1>
+		Hello, {formatName(user)}!
+	</h1>
+};
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+)
