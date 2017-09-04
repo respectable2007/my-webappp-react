6ebf3d4b7449d9c2c302962a338c273894f74f95
System Administrator <root@zhanghuideMacBook-Air.local>
2017-09-04 10:33:59 +0800

    react

--- src/index.js
+++ src/index.js
@@ -110,11 +110,12 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null)
 			}],
+			stepNumber: 0,
 			xIsNext: true,
 		}
 	}
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
@@ -125,18 +126,28 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
 		});
 	}
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) ? false : true
+		});
+	}
 	render() {
-		const history = this.state.history;
-		const current = history[history.length - 1];
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 		const moves = history.map((step, move) => {
 			const desc = move ? 'Move #' + move : 'Game start';
 			return (
-				<li>
-					<a href="#" onClick={()=>this.jumpTo(move)}>{desc}</a>
+				<li key={move}>
+					<a href="#" 
+					   onClick={()=>this.jumpTo(move)}>
+					   {desc}
+				    </a>
 				</li>
 			);
 		})
